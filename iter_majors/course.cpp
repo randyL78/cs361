a9fe59eafaa5ed00ed01bd5101f92f5540df485b
Randy Layne <randydavidl78@gmail.com>
2020-05-24 08:53:57 -0400

    Add prev assignment course.cpp and course.h

---
+++ iter_majors/course.cpp
@@ -0,0 +1,148 @@
+#include "course.h"
+
+#include <cassert>
+#include <iostream>
+#include <string>
+#include <utility>
+#include <algorithm>
+
+using namespace std;
+
+
+/**
+ * Create a new course and set cname to null
+ */
+Course::Course()
+        : name(), numberOfPrerequisites(0), maxPrerequisites(10),
+          prereqs(new CourseName[maxPrerequisites])
+{
+
+}
+
+/**
+ * Create a new course named cname and having no
+ * known prerequisites;
+ */
+Course::Course (const CourseName& cname)
+        : name(cname), numberOfPrerequisites(0), maxPrerequisites(10),
+          prereqs(new CourseName[maxPrerequisites])
+{
+
+}
+
+/**
+ * Create a deep copy of a course
+ * @param course - the course to copy
+ */
+Course::Course(const Course &course)
+        : name(course.getName()), numberOfPrerequisites(course.getNumberOfPrereqs()),
+          maxPrerequisites(10), prereqs(new CourseName[maxPrerequisites])
+{
+  for (int i = 0; i < numberOfPrerequisites; i++) {
+    prereqs[i] = course.getPrereq(i);
+  }
+}
+
+/**
+ * Move constructor
+ * @param course
+ */
+Course::Course(Course &&course)
+        : name(course.name), numberOfPrerequisites(course.numberOfPrerequisites),
+          maxPrerequisites(10), prereqs(course.prereqs)
+{
+  course.prereqs = nullptr;
+}
+
+Course::~Course() {
+  delete [] prereqs;
+}
+
+/**
+ * @return the number of known prerequisites to this course.
+ */
+int Course::getNumberOfPrereqs() const
+{
+  return numberOfPrerequisites;
+}
+
+/**
+ * Adds cname to the list of prerequisites for this course.
+ * If this name is already in the list, this has no effect.
+ *
+ * @param cname name of a prerequisite course
+ */
+void Course::addPrereq(const CourseName& cname)
+{
+  for (int i = 0; i < numberOfPrerequisites; ++i)
+    if (prereqs[i] == cname)
+      return;
+  assert (numberOfPrerequisites < maxPrerequisites);
+  prereqs[numberOfPrerequisites] = cname;
+  ++numberOfPrerequisites;
+}
+
+/**
+ * Removes cname from the list of prerequisites for this course.
+ * If this name is not in the list, this has no effect.
+ *
+ * @param cname name of a prerequisite course
+ */
+void Course::removePrereq(const CourseName& cname)
+{
+  for (int i = 0; i < numberOfPrerequisites; ++i)
+    if (prereqs[i] == cname)
+    {
+      for (int j = i+1; j < numberOfPrerequisites; ++j)
+        prereqs[j-1] = prereqs[j];
+      --numberOfPrerequisites;
+      break;
+    }
+}
+
+/**
+ * Get the name of the i_th prerequisite.
+ *
+ * @precondition 0 <= i && i < getNumberofPrereqs()
+ * @param i index of the prerequisite to retrieve
+ * @return name of the prerequisite course
+ */
+CourseName Course::getPrereq(int i) const
+{
+  assert (i >= 0 && i < numberOfPrerequisites);
+  return prereqs[i];
+}
+
+const Course &Course::operator=(const Course &rhs)
+{
+  Course copy = rhs;
+  std::swap(*this, copy);
+
+  return *this;
+}
+
+Course &Course::operator=(Course &&rhs)
+{
+  std::swap(name, rhs.name);
+  std::swap(numberOfPrerequisites, rhs.numberOfPrerequisites);
+  std::swap(prereqs, rhs.prereqs);
+
+  return *this;
+}
+
+bool operator==(const Course &lhs, const Course &rhs)
+{
+  return lhs.getName() == rhs.getName() &&
+         lhs.getNumberOfPrereqs() == rhs.getNumberOfPrereqs();
+}
+
+bool operator<(const Course &lhs, const Course &rhs) {
+  if (lhs.getName() == rhs.getName() &&
+      lhs.getNumberOfPrereqs() < rhs.getNumberOfPrereqs()) {
+    return true;
+  }
+  if (lhs.getName() < rhs.getName()) {
+    return true;
+  }
+  return false;
+}
---
+++ iter_majors/course.h
@@ -0,0 +1,122 @@
+#ifndef COURSE_H
+#define COURSE_H
+
+#include <iostream>
+#include <string>
+#include "courseName.h"
+
+using namespace std;
+
+class Course {
+    //** You may not change the declarations in this private: area.
+    CourseName name;           ///< Name of the course
+    int numberOfPrerequisites; ///< How many prereqs are currently in the array?
+    int maxPrerequisites;      ///< How many prereqs will fit into the array?
+    CourseName * prereqs;      ///< Pointer to an array of course names.
+public:
+
+    /**
+     * Create a new course and set cname to null
+     */
+    Course();
+
+    /**
+     * Create a new course named cname and having no
+     * known prerequisites;
+     */
+    Course (const CourseName& cname);
+
+    /**
+     * Create a deep copy of a course
+     * @param course - the course to copy
+     */
+    Course(const Course &course);
+
+    /**
+     * Move constructor
+     * @param course
+     */
+    Course(Course &&course);
+
+    /**
+     * Just your run of the mill destructor
+     */
+    ~Course();
+
+    /**
+     * @return the name of the course
+     */
+    CourseName getName() const {return name;}
+
+    /**
+     * @return the number of known prerequisites to this course.
+     */
+    int getNumberOfPrereqs() const;
+
+    /**
+     * Adds cname to the list of prerequisites for this course.
+     * If this name is already in the list, this has no effect.
+     *
+     * @param cname name of a prerequisite course
+     */
+    void addPrereq(const CourseName& cname);
+
+    /**
+     * Removes cname from the list of prerequisites for this course.
+     * If this name is not in the list, this has no effect.
+     *
+     * @param cname name of a prerequisite course
+     */
+    void removePrereq(const CourseName& cname);
+
+    /**
+     * Get the name of the i_th prerequisite.
+     *
+     * @precondition 0 <= i && i < getNumberofPrereqs()
+     * @param i index of the prerequisite to retrieve
+     * @return name of the prerequisite course
+     */
+    CourseName getPrereq(int i) const;
+
+    /**
+     * Assigns one Course to another (Copy Assignment)
+     * @param rhs the Course to be assigned
+     * @return
+     */
+    const Course& operator=(const Course &rhs);
+
+    /**
+     * Assigns one Course to another (Move Assignment)
+     * @param rhs
+     * @return
+     */
+    Course& operator=(Course &&rhs);
+
+
+
+private:
+    friend std::ostream& operator<< (std::ostream& out, const Course& c);
+};
+
+/**
+ * Compares two courses for equality
+ * @param lhs
+ * @param rhs
+ * @return
+ */
+bool operator==(const Course &lhs, const Course &rhs);
+
+/**
+ * Compares the CourseName of 2 Courses
+ * @param lhs
+ * @param rhs
+ * @return true if lhs CourseName is less thant rhs CourseName
+ */
+bool operator<(const Course &lhs, const Course &rhs);
+
+
+
+std::ostream& operator<< (std::ostream& out, const Course& c);
+
+
+#endif

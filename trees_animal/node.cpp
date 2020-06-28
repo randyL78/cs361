ab2211951fa75dc3f5fe8f1637f434e129447d91
Randy Layne <randydavidl78@gmail.com>
2020-06-28 15:52:31 -0400

    Create write function for node.cpp

--- trees_animal/node.cpp
+++ trees_animal/node.cpp
@@ -59,13 +59,45 @@ using namespace std;
 //   that read(...) will accept, recreating the original tree.
 void node::write (std::ostream& out, const node* root)
 {
-  // insert your code here
+  // ensure root isn't null
+  if(root == nullptr)
+    return;
+
+  // determine if node is question or answer
+  if(root->ifNo == nullptr && root->ifYes == nullptr)
+    out << "A ";
+  else
+    out << "Q ";
+
+  // write details to stream
+  out << root->detailText << endl;
+
+  // call left and right nodes
+  write(out, root->ifNo);
+  write(out, root->ifYes);
 }
 
 
 // read a tree from in storing the tree root in t
 void node::read (istream& in, node*& t)
 {
-  // replace the line below by your own code
+  // exit if end of file reached
+  if (in.eof()) {
+    t = NULL;
+    return;
+  }
+
+  // import a line of text
+  string text = "";
+  getline(in, text);
+
+  // get the first letter of the incoming string
+  char type = text[0];
+
+  // trim the leading whitespace after the first letter and save as
+  // the nodes detailText
+  text = text.substr(1);
+  text = text.substr(text.find_first_not_of(' '));
+
   t = NULL;
 }

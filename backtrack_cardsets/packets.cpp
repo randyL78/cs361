b6e7cf5c9d8984f5d43e4d2b6a93ad5e05662fd1
Randy Layne <randydavidl78@gmail.com>
2020-07-22 23:27:31 -0400

    Copy in Dr Zeil's solution

--- backtrack_cardsets/packets.cpp
+++ backtrack_cardsets/packets.cpp
@@ -1,91 +1,85 @@
 #include "packets.h"
 
 #include <vector>
-#include <set>
 
 #include <algorithm>
-#include <iostream>
 #include "backtrack.h"
 
 using namespace std;
 
-
-// Returns the maximum packet value in the collection
-int findMax(const Collection& allPackets)
-{
-  int max = 0;
-  // quadratic time... not the model of efficiency I know.
-  for (Packet packet: allPackets)
-    for (int card : packet)
-      if(card > max)
-        max = card;
-
-  return max;
-}
-
-// Returns the number of packets in the solution, or 0 if not a solution
-int checkSolution(BackTrack bt, const Collection& all_packets, int size, int max_card)
-{
-  // Using a set here since it automatically filters out duplicates
-  std::set<int> cards;
-  int number_of_cards = 0;
-
-  for(int i = 0; i < size; ++i) {
-    if(bt[i] == 1) {
-      cards.insert(all_packets[i].begin(), all_packets[i].end());
-      ++number_of_cards;
-    }
-  }
-
-  return (int)cards.size() == max_card ? number_of_cards : 0;
-}
 
 // Sets up the minimumCollectionSize function as a backtracking problem.
 // # of backtracking variables = # of packets
 // Each variable takes on two possible values:
 //    0 = packet is not in the collection
 //    1 = packet is in the collection
-int minimumCollectionSize (const Collection& allPackets)
+
+
+
+
+//
+// Compute the number of cards in a collection.
+//
+int numberOfCardsInCollection (const Collection& purchasedpackets)
 {
-  // Since we are just interested in the number of cards, best to remove
-  // duplicates.
-  // I'm guessing there is a pruning way of doing this, but best I
-  // could come up with was removing duplicates from the vector
-  Collection copyOfPackets(allPackets.begin(), allPackets.end());
-  sort(copyOfPackets.begin(), copyOfPackets.end());
-  auto it = unique(copyOfPackets.begin(), copyOfPackets.end());
-  copyOfPackets.resize(distance(copyOfPackets.begin(), it));
-
-  // Vector to keep track of all the solutions. The minimum value in
-  // the solution set is the one we want to return.
-  vector<int> solutions;
-
-  // Find the number of possible answers by finding unique packet values in the
-  // collection. Since these are aliases for integers and are contiguous, then this
-  // amounts to finding the max value.
-  int max_card = findMax(copyOfPackets);
-
-  // Since we are dealing with a vector that isn't changing, we only want to check
-  // the size once!
-  int all_packets_count = copyOfPackets.size();
-
-  BackTrack bt(all_packets_count, 2);
-
-  // Since there could be multiple solutions, and we want the minimum one, we
-  // don't need to check for solved in the condition.
-  while (bt.more())
+  Packet collection;
+  for (unsigned i = 0; i < purchasedpackets.size(); ++i)
   {
-    // check current bt to see if it is a solution, if it is push the number of cards in
-    // the solution to the solutions vector.
-    int solved = checkSolution(bt, copyOfPackets, all_packets_count, max_card);
-    if(solved > 0)
-      solutions.push_back(solved);
+    Packet theUnion;
 
-    // Since we are storing the solutions in a vector, no need to check for solution
-    // before incrementing.
-    ++bt;
+    set_union (collection.begin(), collection.end(),
+               purchasedpackets[i].begin(), purchasedpackets[i].end(),
+               inserter(theUnion, theUnion.end()));
+    theUnion.swap (collection);
   }
 
-  // make sure solutions isn't empty and then return the minimum number in it.
-  return solutions.begin() == solutions.end() ? 0 : *min_element(solutions.begin(), solutions.end());
+  // How many cards did we end up with?
+  return collection.size();
+}
+
+
+
+int minimumCollectionSize (const Collection& allPackets)
+{
+  int nPackets = allPackets.size();
+  int totalNumberOfCards = numberOfCardsInCollection(allPackets);
+
+  BackTrack backtrack (nPackets, 2);
+  int smallestSolutionSoFar = nPackets;
+
+  while (backtrack.more())
+  {
+    Collection solution;
+    int nPacketsInSolution = 0;
+    for (int i = 0; i < nPackets; ++i)
+    {
+      if (backtrack[i] == 1)
+      {
+        solution.push_back (allPackets[i]);
+        ++nPacketsInSolution;
+      }
+    }
+    int nCards = numberOfCardsInCollection(solution);
+    if (nCards == totalNumberOfCards)
+    {
+      smallestSolutionSoFar = min(smallestSolutionSoFar,
+                                  nPacketsInSolution);
+    }
+
+    // To speed things up, if the number of packets in our solution
+    // is already as large as smallestSolutionSoFar, then there's no
+    // point to trying any solutions that add still more packets, so
+    // we prune at the highest numerbered packet in the collection.
+    if (nPacketsInSolution >= smallestSolutionSoFar)
+    {
+      int maxPacket = 0;
+      for (int i = 0; i < nPackets; ++i)
+        if (backtrack[i] == 1)
+          maxPacket = i;
+      backtrack.prune(maxPacket);
+    }
+    else
+      ++backtrack;
+  }
+  return smallestSolutionSoFar;
 }
